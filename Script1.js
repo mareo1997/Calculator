77e90efd561901f5c01d0bd1592564920f3b2ddb
mareo1997 <mareo1997@gmail.com>
2020-08-01 13:50:27 -0500

    Got numbers continous

---
+++ Script1.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+var total;
+var x = [];
+var display;
+
+function number(clicked) {
+    if (clicked / 1) {
+        x.push(clicked);
+        display = x.join('');
+        document.getElementById("display").innerHTML = display;
+    }
+    if
+    /*if (clicked == "eql") {
+        document.getElementById("display").innerHTML = clicked;
+    }*/
+}
