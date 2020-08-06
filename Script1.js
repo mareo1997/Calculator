636af33016918f50d412f6981554db94519d3913
mareo yapp <mareo1997@gmail.com>
2020-08-06 18:55:18 -0500

    Graphics

--- Script1.js
+++ Script1.js
@@ -1,80 +1,130 @@
 // JavaScript source code
-var total; //25
+var total = null; //25
 var x = [];
 var display;
 var num1; //12
 var num2;//13
 var calc = [];
 
-function number(clicked) {
-    if (typeof clicked == 'number') {
-        x.push(clicked);
+function Calculation(click) {
+    if (typeof click == 'number') {
+        number(click);
+    } else if (click == 'clr') {
+        clear(click);
+    } else {
+        math(click);
+    }
+}
+
+function number(num) {
+    if (total != null && (calc[1] == '+' || calc[1] == '*' || calc[1] == '/' || calc[1] == '-')) {
+        x.push(num);
         document.getElementById("x").innerHTML = x;
         display = Number(x.join(''));
-        document.getElementById("display").innerHTML = display;
-    } else if (clicked == 'add') {
-        add();
-    } else if (clicked == 'mul') {
-        mul();
-    } else if (clicked == 'sub') {
-        if (calc[0] == undefined) {
-            calc.push(display);
-            calc.push('-');
-            document.getElementById("c").innerHTML = calc;
-        } else {
-            calc.push('-');
-            document.getElementById("c").innerHTML = calc;
-        }
-        x = [];
-    } else if (clicked == 'eql') {
-        eql();
-    } else if (clicked == 'clr'){
-        clear();
+        document.getElementById("display").innerHTML = "3 Display: " + display;
+    }else if (total != null) {
+        total = null;
+        calc = [];
+        x.push(num);
+        document.getElementById("x").innerHTML = x;
+        display = Number(x.join(''));
+        document.getElementById("display").innerHTML = "1 Display: " + display; 
+    } else if(total == null){
+        x.push(num);
+        document.getElementById("x").innerHTML = x;
+        display = Number(x.join(''));
+        document.getElementById("display").innerHTML = "2 Display: " + display;
     }
-}*/
-/*function add() {
-    if (calc[0] == undefined) {
+}
+
+function math(symbol) {
+    if (symbol == '+') {
+        add();
+    } else if (symbol == '*') {
+        mul();
+    } else if (symbol == '/') {
+        div();
+    } else if (symbol == '-') {
+        sub();
+    } else if (symbol == '=') {
         calc.push(display);
-        calc.push('+');
-        document.getElementById("c").innerHTML = calc;
-    } else {
-        calc.push('+');
-        document.getElementById("c").innerHTML = calc;
+        eql();
     }
     x = [];
+    document.getElementById("x").innerHTML = x;
+}
+
+function add() {
+    if (total != null) {
+        calc.push('+');
+        document.getElementById("c").innerHTML = "1 Add: " + calc;
+    } else if (total == null) {
+        calc.push(display);
+        calc.push('+');
+        document.getElementById("c").innerHTML = "2 Add: " + calc;
+    }
+}
+
+function sub() { //problem 0-5
+    if (total != null) {
+        calc.pop();
+        calc.push('-');
+        document.getElementById("c").innerHTML = "1 Sub: " + calc;
+    } else if (total == null) {
+        calc.push(display);
+        calc.push('-');
+        document.getElementById("c").innerHTML = "2 Sub: " + calc;
+    }
 }
 
 function mul() {
-    if (calc[0] == undefined) {
+    if (total != null) {
+        calc.push('*');
+        document.getElementById("c").innerHTML = "1 Mul: " + calc;
+    } else if (total == null) {
         calc.push(display);
         calc.push('*');
-        document.getElementById("c").innerHTML = calc;
-    } else {
-        calc.push('*');
-        document.getElementById("c").innerHTML = calc;
+        document.getElementById("c").innerHTML = "2 Mul: " + calc;
     }
-    x = [];
+}
+
+function div() {
+    if (total != null) {
+        calc.push('/');
+        document.getElementById("c").innerHTML = "1 Div: " + calc;
+    } else if (total == null) {
+        calc.push(display);
+        calc.push('/');
+        document.getElementById("c").innerHTML = "2 Div: " + calc;
+    }
 }
 
 function eql() {
-    calc.push(display);
     if (calc[1] == '+') {
         total = calc[0] + calc[2];
     } else if (calc[1] == '*') {
         total = calc[0] * calc[2];
+    } else if (calc[1] == '/') {
+        if (calc[2] == 0) {
+            clear();
+        } else {
+            total = calc[0] / calc[2];
+        }
     } else if (calc[1] == '-') {
         total = calc[0] - calc[2];
+    } else if (calc[0] == '-') {
+        total = 0 - calc[1];
     }
-    document.getElementById("display").innerHTML = total;
+    document.getElementById("display").innerHTML = "Total = " + total;
     calc[0] = total;
-    x = [];
     calc.pop();
     calc.pop();
-    document.getElementById("c").innerHTML = calc;
-}*/
+    document.getElementById("c").innerHTML = "Calc: " + calc;
+}
 
 function clear() {
     calc = [];
     x = [];
+    total = null;
     document.getElementById("display").innerHTML = "Cleared";
 }
